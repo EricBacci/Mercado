0e9ab724ee9f1f8f7f360b9e59587b79638ba010
EricBacci <162828902+EricBacci@users.noreply.github.com>
2024-04-18 07:17:10 -0300

    Add class Order mercado.py
    Adicinado classe Order no código.

--- mercado.py
+++ mercado.py
@@ -47,27 +47,59 @@ class Product:
         })
 
 
-"""Listas iniciais vazias"""
-available_products = []
+class Order:
+    """Modelagem dos pedidos"""
+    def __init__(self, order_number, order_products=[], available_products=[]):
+        """Inicializa o pedido com preço zerado"""
+        self.order_number = order_number
+        self.order_products = order_products
+        self.total_price = 0
+        
+        for product_code in order_products:
+            for product_dict in available_products:
+                if product_code in product_dict:
+                    self.total_price += product_dict[product_code]['price']
+                    break
+
+    def describe_order(self):
+        print(f"Numero do pedido: {self.order_number}")
+        print(f"Lista de itens: {self.order_products}")
+        print(f"Preço do pedido: {self.total_price}")
+
+
+"""Inicia as listas vazias"""
 customer_list = []
+available_products = []
+order_list = []
+
 
 """Instanciando alguns produtos"""
 product_leite = Product(1001, 'Leite', 4.50)
 product_cafe = Product(1002, 'Cafe', 10)
 product_banana = Product(1003, 'Banana', 9)
 
-"""Instanciando alguns clientes"""
-customer_eric = Customer('Eric')
-customer_eduarda = Customer('Eduarda')
-customer_otavio = Customer('Otávio')
+"""Criando um Order e mostrando o valor total"""
+order_1 = Order(1001, [1001, 1002, 1003], available_products)
+order_1.describe_order()
 
-"""Realizando compras"""
-customer_eric.purchase(1001, 1002, 1003)
-customer_eric.purchase(1001)
-customer_eduarda.purchase(1001)
-customer_otavio.purchase(1002, 1003)
+order_2 = Order(1002, [1001, 1001, 1001, 1001, 1001, 1001], available_products)
+order_2.describe_order()
 
-"""Mostrando dados finais de cada usuário"""
-customer_eric.describe_customer()
-customer_eduarda.describe_customer()
-customer_otavio.describe_customer()
+# """Instanciando alguns clientes"""
+# customer_eric = Customer('Eric')
+# customer_eduarda = Customer('Eduarda')
+# customer_otavio = Customer('Otávio')
+
+# """Realizando compras"""
+# customer_eric.purchase(1001, 1002, 1003)
+# customer_eric.purchase(1001)
+# customer_eduarda.purchase(1001)
+# customer_otavio.purchase(1002, 1003)
+
+# """Mostrando dados finais de cada usuário"""
+# customer_eric.describe_customer()
+# customer_eduarda.describe_customer()
+# customer_otavio.describe_customer()
+
+# """Mostrando lista de itens"""
+# print(available_products)
